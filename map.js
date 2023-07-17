d90779a7eb89d84116269c33a366d0ee9f1c8fe3
cjns <67065839+cjns@users.noreply.github.com>
2023-07-17 23:24:19 +0100

    The map method

--- map.js
+++ map.js
@@ -1,20 +1,32 @@
-  
+
 /**
  * To run this file in Gitpod, use the 
  * command node map.js in the terminal
  */
 
 // Using a for loop
-
+let nums = [1, 2, 3, 4, 5];
+let results = [];
+for (let num of nums) {
+  results.push(num * 2);
+}
+console.log(results);
 
 // Using map()
+const multByTwo = function (num) {
+  return num * 2;
+};
 
+const mapResults = nums.map(multByTwo);
+console.log(mapResults);
 
 // Simplified w/ map()
-
+const simplified = nums.map(function (num) { return num * 2 });
+console.log(simplified);
 
 // Simplfied w/ map() + arrow function
-
+const arrow = nums.map(num => num * 2)
+console.log(arrow);
 
 // With objects:
 const students = [
@@ -37,3 +49,17 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+const studentsWithIds = students.map(student => [student.name, student.id]);
+console.log(studentsWithIds);
+
+const studentObjectsWithIds = students.map(student => {
+  let { id, name } = student;
+  return { id, name };
+});
+console.log(studentObjectsWithIds);
+
+const studentsWithAge = students.map(student => {
+  return { ...student, age: Math.floor(Math.random() * 100) };
+});
+console.log(studentsWithAge);
